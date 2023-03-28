903acf1ff0e04368422be86f5905d148c2b100d1
Itzel Cabrera <64556979+ItzelCabrera@users.noreply.github.com>
2023-03-27 22:28:59 -0600

    Delete calc_local.h

--- calc_local.h
+++
@@ -1,8 +0,0 @@
-//archivo cabecera
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-int suma(int a,int b);
-int resta(int a,int b);
-int multiplicacion(int a,int b);
-int division(int a,int b);
